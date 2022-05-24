2336547f337cbc0866c9748657d3f74869781b97
kazuhiro-na <narakhope@gmail.com>
2022-05-24 19:13:50 +0900

    名前入力のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  
+  validates :nickname, presence: true
+  validates :birthday, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :name_sei
+    validates :name_mei
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :name_sei_kana 
+    validates :name_mei_kana
+  end
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
-  validates :nickname, presence: true
-  validates :name_sei, presence: true
-  validates :name_mei, presence: true
-  validates :name_sei_kana, presence: true
-  validates :name_mei_kana, presence: true
-  validates :birthday, presence: true
 
   
 end
