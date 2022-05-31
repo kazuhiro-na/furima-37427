2cf26ab7821e5177d07a58964e1a7ac44bb64863
kazuhiro-na <narakhope@gmail.com>
2022-05-31 14:27:50 +0900

    価格のバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,5 +16,6 @@ class Item < ApplicationRecord
   validates :burden_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
   
 end
