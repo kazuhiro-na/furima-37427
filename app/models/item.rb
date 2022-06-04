3d1c778ab9e5222bec541d5d4058ec07647176cc
kazuhiro-na <narakhope@gmail.com>
2022-06-04 21:09:52 +0900

    priceのバリデーションの変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,6 @@ class Item < ApplicationRecord
   validates :burden_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, numericality: { in:300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
-  
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } 
+  validates :price, inclusion: { in: 300..9_999_999, message: "is out of setting range" } 
 end
