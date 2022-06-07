8ccc8fbf462ec9e31d7a703cfc378aa866c63702
kazuhiro-na <narakhope@gmail.com>
2022-06-07 22:38:16 +0900

    itemモデルのFactoryBotを作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :image, presence: true
   validates :explanation, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :burden_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :burden_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
   validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' } 
   validates :price, inclusion: { in: 300..9_999_999, message: "is out of setting range" } 
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Commerce.product_name }
+    explanation { Faker::Lorem.paragraph }
+    category { Category.all.sample }
+    condition { Condition.all.sample }
+    burden { Burden.all.sample }
+    prefecture { Prefecture.all.sample }
+    shipping_day { ShippingDay.all.sample }
+    price { Faker::Commerce.price(range:300..9_999_999,as_string:true)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
