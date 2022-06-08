c30954bc15c3bd9afa3632833c655b1cdf36ea97
kazuhiro-na <narakhope@gmail.com>
2022-06-09 01:47:45 +0900

    テストコードの修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,41 +27,47 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
-      it 'カテゴリーの情報が空では出品できない' do
-        @item.category = nil
+      it 'カテゴリーに「---」が選択されているときは出品できない' do
+        @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
       it '商品の状態の情報が空では出品できない' do
-        @item.condition = nil
+        @item.condition_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Condition can't be blank")
       end
       it '配送料の負担の情報が空では出品できない' do
-        @item.burden = nil
+        @item.burden_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Burden can't be blank")
       end
       it '発送元の地域の情報が空では出品できない' do
-        @item.prefecture = nil
+        @item.prefecture_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Prefecture can't be blank")
       end
       it '発送までの日数の情報が空では出品できない' do
-        @item.shipping_day = nil
+        @item.shipping_day_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping day can't be blank")
       end
       it '価格が空では出品できない' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid. Input half-width characters',
+        expect(@item.errors.full_messages).to include('Price is not a number',
                                                       'Price is out of setting range')
       end
       it '価格が全角数字では出品できない' do
         @item.price = '１２３４'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is out of setting range')
+        expect(@item.errors.full_messages).to include('Price is not a number',
+                                                      'Price is out of setting range')
+      end
+      it '価格が少数では出品できない' do
+        @item.price = '1234.5'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price must be an integer')
       end
       it '価格が299円以下では出品できない' do
         @item.price = '299'
