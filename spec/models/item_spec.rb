e673383494cee335c5063b19bbe1e245c8674269
kazuhiro-na <narakhope@gmail.com>
2022-06-08 19:08:33 +0900

    itemモデルの単体テストコードを実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,79 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
+
+  describe '商品出品' do
+    context '商品出品できるとき' do
+      it '必要な情報を適切に入力すれば商品出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できないとき' do
+      it '画像が空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it '商品名が空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明が空では出品できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'カテゴリーの情報が空では出品できない' do
+        @item.category = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の状態の情報が空では出品できない' do
+        @item.condition = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it '配送料の負担の情報が空では出品できない' do
+        @item.burden = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Burden can't be blank")
+      end
+      it '発送元の地域の情報が空では出品できない' do
+        @item.prefecture = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it '発送までの日数の情報が空では出品できない' do
+        @item.shipping_day = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+      end
+      it '価格が空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is invalid. Input half-width characters", "Price is out of setting range")
+      end
+      it '価格が全角数字では出品できない' do
+        @item.price = '１２３４'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it '価格が299円以下では出品できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it '価格が10,000,000円以上では出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'ユーザーが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
