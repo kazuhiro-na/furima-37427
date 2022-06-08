b319d206303e710b44d35372a2ae13479edd9579
kazuhiro-na <narakhope@gmail.com>
2022-06-08 21:41:04 +0900

    価格が少数では保存できないようバリデーションを変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,5 @@ class Item < ApplicationRecord
   validates :burden_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_day_id, presence: true
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'is invalid. Input half-width characters' }
-  validates :price, inclusion: { in: 300..9_999_999, message: 'is out of setting range' }
+  validates :price, numericality: { only_integer: true }, inclusion: { in: 300..9_999_999, message: 'is out of setting range' }
 end
