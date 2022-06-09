2a3f13f2e5c030ba16fd75426280abb157282fbb
kazuhiro-na <narakhope@gmail.com>
2022-06-09 11:43:56 +0900

    販売利益の小数点以下の切り捨てを追加

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -2,7 +2,7 @@ function price (){
   const itemPrice = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
     tax = Math.floor(itemPrice.value * 0.1);
-    saleProfit = itemPrice.value - tax
+    saleProfit = Math.floor(itemPrice.value - tax)
     const addTaxPrice = document.getElementById("add-tax-price")
     const profit = document.getElementById("profit")
     addTaxPrice.innerHTML = `${tax}`;
