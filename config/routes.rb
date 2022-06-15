d1d988c60c69b73eae7e9e9f9204d68084ac981e
kazuhiro-na <narakhope@gmail.com>
2022-06-15 14:18:16 +0900

    ルーティングをネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :purchase_records, only: [:index, :create]
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :purchase_records, only: [:index, :create]
+  end
 end
