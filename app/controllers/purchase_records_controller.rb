7143efd548736ed0fd0f94e6ccd9832bf24a24c1
kazuhiro-na <narakhope@gmail.com>
2022-06-15 14:29:05 +0900

    購入画面に商品が表示されるよう実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,6 +1,7 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!, only: :index
   def index
+    @item = Item.find(params[:item_id])
   end
 
   def create
