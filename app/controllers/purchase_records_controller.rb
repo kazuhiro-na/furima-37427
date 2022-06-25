7b57cee6e2425b129a8d398ab8fdd5d9d84a3921
kazuhiro-na <narakhope@gmail.com>
2022-06-25 20:52:07 +0900

    重複したコードをbefore_actionで呼び出す形に修正

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,13 +1,12 @@
 class PurchaseRecordsController < ApplicationController
   before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
   def index
-    @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new
     redirect_to root_path if @item.purchase_record.present? || @item.user_id == current_user.id
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @record_address = RecordAddress.new(record_address_params)
     if @record_address.valid?
       pay_item
@@ -24,6 +23,10 @@ class PurchaseRecordsController < ApplicationController
     params.require(:record_address).permit(:tell, :postal_code, :prefecture_id, :municipalities, :address, :building).merge(
       user_id: current_user.id, item_id: params[:item_id], token: params[:token]
     )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
   def pay_item
