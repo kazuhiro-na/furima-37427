eb814f158261f112a6af6709fdcf12b16e44f25f
kazuhiro-na <narakhope@gmail.com>
2022-06-26 13:54:14 +0900

    Revert "修正"
    This reverts commit 0edef5c28d7568492b74110eb5aa0dbd4ad2df56.

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Furima37427
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    Faker::Config.locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
@@ -18,3 +18,4 @@ module Furima37427
   end
 end
 
+Faker::Config.locale = :ja
